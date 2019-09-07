4ff57f9b17559edbda9a9943e3ca7c97bffea982
salmankarimli <55030195+salmankarimli@users.noreply.github.com>
2019-09-07 22:23:21 +0400

    Add files via upload

---
+++ ders2.py
@@ -0,0 +1,4 @@
+f=open('E:\Ders\ders1.py')
+N=f.readlin()
+print(N)
+f.close()
---
+++ p2.py
@@ -0,0 +1,3 @@
+i=int(input("Rəqəmi daxil edin "))
+print('Daxil etdiyiniz rəqəm {0}, rəqəmdən əvvəlki ədəd {1}, sonrakı rəqəm {2}
+'.format(i,i-1,i+1))
