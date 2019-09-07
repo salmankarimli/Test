64370bafd9ae2da74aae04f371aebe08eeaef9d7
salmankarimli <55030195+salmankarimli@users.noreply.github.com>
2019-09-07 22:35:34 +0400

    Delete ders1.py

--- ders1.py
+++
@@ -1,8 +0,0 @@
-name=input("Salam, Zəhmət olmasa adınızı daxil edin : ")
-il=int(input("Təvəllüdü (il) daxil edin : "))
-ay=int(input("Təvəllüdü (ay) daxil edin : "))
-gun=int(input("Təvəllüdü (gun) daxil edin : "))
-print ("Salam "+name)
-print ("Sizin təvəllüdünüz ",gun,".",ay,".",il)
-if (il==1977 and ay==6 and gun==25):
-	print ("Eyni tevellud "*3)
